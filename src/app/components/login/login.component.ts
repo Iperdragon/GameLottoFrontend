70c279af2f9572e13e5c1539d9696cc79fb6f803
Draco <dracovelardi7@gmail.com>
2025-01-22 15:40:17 +0100

    Solo Login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {CredentialServiceService} from '../../services/credential-service.service';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './login.component.html',
   standalone: true,
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent
+{
+  constructor(private servizioCredenziali:CredentialServiceService, private http:HttpClient) {
+  }
 
+  credenziali={username:"", password:""};
+
+  login()
+  {
+    this.servizioCredenziali.login(this.credenziali)
+  }
 }

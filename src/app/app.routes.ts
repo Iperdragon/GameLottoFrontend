b324fcaf9685d4d9aac860eca14455f0a9cc17b0
AkiSora25 <marco3356@outlook.it>
2025-01-23 17:30:05 +0100

    ..

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import {LoginComponent} from './components/login/login.component';
 import {RegisterComponent} from './components/register/register.component';
 import {HomepageComponent} from './components/homepage/homepage.component';
 import {PaginaCoverComponent} from './components/pagina-cover/pagina-cover.component';
+import {PaginaGlossaryComponent} from './components/pagina-glossary/pagina-glossary.component';
 
 export const routes: Routes = [
   {path:"login",component:LoginComponent},
   {path:"register",component:RegisterComponent},
   {path:'',component:HomepageComponent},
-  {path: 'cover',component:PaginaCoverComponent}
+  {path: 'cover',component:PaginaCoverComponent},
+  {path: 'glossary',component:PaginaGlossaryComponent}
 
 ];

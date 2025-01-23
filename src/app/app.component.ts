dbbc8280264fb43989e7637ed2cb0c002af71d00
AkiSora25 <marco3356@outlook.it>
2025-01-23 17:16:20 +0100

    ..

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {RouterLink, RouterOutlet} from '@angular/router';
 import {LoginComponent} from './components/login/login.component';
+import {PaginaGlossaryComponent} from './components/pagina-glossary/pagina-glossary.component';
 
 @Component({
   selector: 'app-root',
-    imports: [RouterOutlet, LoginComponent, RouterLink],
+  imports: [RouterOutlet, LoginComponent, RouterLink, PaginaGlossaryComponent],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.css'

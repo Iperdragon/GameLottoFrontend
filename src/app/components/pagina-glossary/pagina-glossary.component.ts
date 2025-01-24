8e5a8a06df301b52b5c1d711300a9106fd264549
AkiSora25 <marco3356@outlook.it>
2025-01-24 10:28:03 +0100

    glossary

--- src/app/components/pagina-glossary/pagina-glossary.component.ts
+++ src/app/components/pagina-glossary/pagina-glossary.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-pagina-glossary',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './pagina-glossary.component.html',
   standalone: true,
   styleUrl: './pagina-glossary.component.css'

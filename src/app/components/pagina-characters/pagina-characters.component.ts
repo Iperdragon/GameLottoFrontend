9ec182a25b4b113e61ec01754a4e3bdea6f5e7fa
Draco <dracovelardi7@gmail.com>
2025-01-26 13:26:06 +0100

    Mod personaggi completa al 95%

--- src/app/components/pagina-characters/pagina-characters.component.ts
+++ src/app/components/pagina-characters/pagina-characters.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, Renderer2} from '@angular/core';
 import {VideogameDTORespCover} from '../../model/VideogameDTORespCover';
 import {PGDTOResp} from '../../model/PGDTOResp';
 import {RoundLoaderService} from '../../services/round-loader.service';
 import {FormsModule} from '@angular/forms';
 import {VideogameDTORespCompl} from '../../model/VideogameDTORespCompl';
+import {NgForOf, NgIf} from '@angular/common';
+import {AutofillerService} from '../../services/autofiller.service';
 
 @Component({
   selector: 'app-pagina-characters',
   imports: [
-    FormsModule
+    FormsModule,
+    NgForOf,
+    NgIf
   ],
   templateUrl: './pagina-characters.component.html',
   standalone: true,
@@ -16,13 +20,18 @@ import {VideogameDTORespCompl} from '../../model/VideogameDTORespCompl';
 })
 export class PaginaCharactersComponent
 {
-  maxStep:number=9;
+  hearts: string[] = Array(5).fill('https://i.postimg.cc/KjHzc8yt/HEART1.png');
+  mostraReStart: boolean = false;
+  mostraProssimo: boolean = false;
+  mostraDescrizione:boolean=false;
+  maxStep:number=5;
   step:number=0;
   idsUsed:number[]=[];
   pg:PGDTOResp|null=null;
   answer:string="";
 
-  constructor(private loader:RoundLoaderService)
+  constructor(private loader:RoundLoaderService,
+              private auto:AutofillerService,)
   {
     this.caricaRound4();
   }
@@ -35,6 +44,9 @@ export class PaginaCharactersComponent
       {
         this.step=0;
         this.pg=res as PGDTOResp;
+        this.mostraProssimo = false;
+        this.mostraDescrizione=false;
+        this.hearts=Array(5).fill('https://i.postimg.cc/KjHzc8yt/HEART1.png')
       }
     )
   }
@@ -44,16 +56,19 @@ export class PaginaCharactersComponent
     if(this.pg!.game==this.answer)
     {
       this.idsUsed.push(this.pg!.id!);
-      this.caricaRound4();
+      this.mostraDescrizione=true;
+      this.mostraProssimo= true;
     }
     else
     {
-      if(this.step<this.maxStep)
+      if(this.step<this.maxStep-1)
       {
+        this.hearts[this.step] = 'https://i.postimg.cc/MZPXmdjX/HEART2.png'
         this.step++;
       }
       else
       {
+        this.hearts[this.step] = 'https://i.postimg.cc/MZPXmdjX/HEART2.png'
         this.terminaGame2();
       }
     }
@@ -61,6 +76,28 @@ export class PaginaCharactersComponent
 
   private terminaGame2()
   {
+    alert('Game Over!');
+    this.mostraReStart = true;
+  }
 
+  restartGame2(): void {
+    this.idsUsed = [];
+    this.step=0;
+    this.mostraReStart=false;
+    this.caricaRound4();
+  }
+
+  filteredOptions: string[] = [];
+
+  onInputChange(value: string): void {
+    this.answer = value;
+    this.filteredOptions = this.auto.frasi.filter(option =>
+      option.toLowerCase().includes(value.toLowerCase())
+    );
+  }
+
+  selectOption(option: string): void {
+    this.answer = option;
+    this.filteredOptions = [];
   }
 }
